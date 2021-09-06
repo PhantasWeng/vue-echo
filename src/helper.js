b39c5c8f5801aa118c112127e054b95f25ade3ef
Phantas Weng <bensonuong@gmail.com>
2021-09-06 21:45:47 +0800

    Add getChannel and error warning

--- src/helper.js
+++ src/helper.js
@@ -1,4 +1,7 @@
 export function getChannel (channels, channelName) {
   const target = Object.keys(channels).find(channel => channels[channel].name.replace('private-', '') === channelName)
+  if (!target) {
+    throw new Error(`[Echo] Channel name not exist: ${channelName}`)
+  }
   return channels[target]
 }
